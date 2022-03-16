c104faabe48e87aadf49083434d90ce8538a6349
ztashaz <nata.zagareva@mail.ru>
2022-03-16 17:19:10 +0300

    add project

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
